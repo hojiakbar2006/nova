cbdefbf8fc43ad043fe15b25bcf69063987023fe
Hojiakbar_Orifov <togarakweb@gmail.com>
2025-09-30 12:42:14 +0500

    Refactor App component: move body content inside the div

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ export default function App() {
     <div>
       hello world
       solar system
+      <body>
+        sdfbsd
+        scsdfsjdS/
+        <input type="text" name="" id="" />
+      </body>
     </div>
   )
 }
