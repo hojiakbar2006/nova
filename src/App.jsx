9f65a04e85f0fc6acf50b6ddc6e81613defd9e34
Hojiakbar_Orifov <togarakweb@gmail.com>
2025-09-30 12:54:20 +0500

    Add input field to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
     <div>
       hello world
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae neque aperiam sint soluta suscipit quaerat consequatur qui iure, corrupti totam debitis aspernatur nobis cum doloremque eligendi non blanditiis eum officiis.
+<input type="text" placeholder="heelllsd" />
     </div>
   )
 }
