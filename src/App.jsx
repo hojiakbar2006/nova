58c14baab8f686bfcd0c1e1418c98505a4ba7245
Hojiakbar_Orifov <togarakweb@gmail.com>
2025-09-30 12:51:47 +0500

    Remove unnecessary elements and clean up App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ export default function App() {
   return (
     <div>
       hello world
-      solar system
-      <body>
-        sdfbsd
-        scsdfsjdS/
-        <input type="text" name="" id="" />
-      </body>
+
     </div>
   )
 }
